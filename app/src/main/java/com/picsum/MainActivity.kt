68145622eb17f6f07fbf6631c47655c47ab040db
@SantiiHoyos <santiago.hoyos93@gmail.com>
2020-05-05 14:03:26 +0200

    Merge de Rama1 (#1)
    * Cambio 1 en rama 1
    
    * Cambio 2 en Rama 1

--- app/src/main/java/com/picsum/MainActivity.kt
+++ app/src/main/java/com/picsum/MainActivity.kt
@@ -9,12 +9,18 @@ import com.picsum.filelist.view.FileListFragment
  */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Cambio 2 en Rama 1
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showFileList()
     }
 
+    /**
+     * Change on Rama1
+     */
     private fun showFileList() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainActivity_fragment_container, FileListFragment())
