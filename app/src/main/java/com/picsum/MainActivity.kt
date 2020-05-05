974207c894f4dafe3acf1d56e7e8082583149cfb
Santi Hoyos <santiago.hoyos93@gmail.com>
2020-05-05 13:53:58 +0200

    Este es un comit por encima de rama 1

--- app/src/main/java/com/picsum/MainActivity.kt
+++ app/src/main/java/com/picsum/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.picsum.filelist.view.FileListFragment
 
+/**
+ * Cambio en master
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
