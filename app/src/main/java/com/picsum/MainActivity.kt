9e182a56bdff1dfd41b0e9e850d36543a353bf37
@SantiiHoyos <santiago.hoyos93@gmail.com>
2020-05-05 17:27:59 +0200

    PR de rama 2 (#2)
    * cambio 1 en rama 2
    
    * cambio 2 en rama 2

--- app/src/main/java/com/picsum/MainActivity.kt
+++ app/src/main/java/com/picsum/MainActivity.kt
@@ -5,19 +5,13 @@ import android.os.Bundle
 import com.picsum.filelist.view.FileListFragment
 
 class MainActivity : AppCompatActivity() {
-
-    /**
-     * Cambio 2 en Rama 1
-     */
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showFileList()
     }
 
-    /**
-     * Change on Rama1
-     */
     private fun showFileList() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainActivity_fragment_container, FileListFragment())
