1aef547912a74c5ed3785a2e57ccec795bb82f43
@SantiiHoyos <santiago.hoyos93@gmail.com>
2020-05-05 17:41:15 +0200

    Rama3 (#3)
    * cambio 1 en rama 3
    
    * cambio 2 en rama 3

--- app/src/main/java/com/picsum/MainActivity.kt
+++ app/src/main/java/com/picsum/MainActivity.kt
@@ -8,13 +8,19 @@ import com.picsum.filelist.view.FileListFragment
  * cambio en master 3
  */
 class MainActivity : AppCompatActivity() {
-    
+
+    /**
+     * cambio 2 en rama 3
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         showFileList()
     }
 
+    /**
+     * cambio 1 en rama 3
+     */
     private fun showFileList() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.mainActivity_fragment_container, FileListFragment())
